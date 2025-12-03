a445556d064d67ce0361fe220219f818a51afe66
dann1663 <dannid148@gmail.com>
2025-12-03 17:30:39 +0100

    update

---
+++ food_delivery/earning_max_behaviour.py
@@ -0,0 +1,23 @@
+from fooddelivery import DrivverBehaviour
+class EarningMaxBehavior(DriverBehaviour):
+    def __init__(self, min_ratio):
+        self.min_ratio = min_ratio
+
+    def decide(
+        self,
+        driver: Driver,
+        offer: Offer,
+        time: int
+    ) -> bool:
+
+        travel_time = offer.estimated_travel_time
+        
+        if travel_time > 0:
+            ratio = offer.estimated_reward / travel_time
+        else:
+            ratio =  int('inf')
+
+        return ratio >= self.min_ratio
+
+
+     
--- phase2/fooddelivery/driver_behaviour.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-class DriverBehavior(ABC):
-
-    @abstractmethod
-    def decide(self, driver: Driver, offer: Offer, time: int) -> Bool:
-        pass  
