3ecef597767829fdd1622d8d1d209e98ee81f679
dann1663 <dannid148@gmail.com>
2025-12-05 13:04:52 +0100

    update

--- phase2/lazy_behaviour.py
+++ phase2/lazy_behaviour.py
@@ -1,4 +1,4 @@
-from food_delivery.driver_behaviour import DriverBehaviour
+from phase.driver_behaviour import DriverBehaviour
 
 class LazyBehaviour(DriverBehaviour):
     def __init__(self, max_idle)-> None:
