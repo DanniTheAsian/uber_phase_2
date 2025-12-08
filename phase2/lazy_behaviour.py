1c782d9ad1b97eb0e5beec5027f6df444547d27d
Thien Ly Buu Huynh <dummy.xiu@gmail.com>
2025-12-08 13:35:06 +0100

    added path for driver and offer into lazy_behaviour

--- phase2/lazy_behaviour.py
+++ phase2/lazy_behaviour.py
@@ -1,8 +1,10 @@
-from phase2.driver_behaviour import DriverBehaviour
+from .behaviour.driver_behaviour import DriverBehaviour
+from .driver import Driver
+from .offer import Offer
 
 class LazyBehaviour(DriverBehaviour):
     def __init__(self, max_idle)-> None:
         self.max_idle = max_idle
 
-    def decide(self, driver: "Driver", offer: "Offer", time: int) -> bool:
+    def decide(self, driver: Driver, offer: Offer, time: int) -> bool:
         return offer.request.wait_time >= self.max_idle
