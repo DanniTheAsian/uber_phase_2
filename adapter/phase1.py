2dd7458223d658c05242f88bf32115d53dd1e60f
Thien Ly Buu Huynh <dummy.xiu@gmail.com>
2025-12-11 16:59:15 +0100

    genereate requests done!

--- adapter/phase1.py
+++ adapter/phase1.py
@@ -3,6 +3,7 @@ from .adapter import SimulationAdapter
 from phase2.driver import Driver
 from phase2.point import Point
 from phase2.request import Request
+from phase2.request_generator import RequestGenerator
 import random
 
 
@@ -25,7 +26,19 @@ def generate_drivers(n: int, width: int, height: int) -> List[Driver]:
     return drivers
 
 def generate_requests(start_t: int, out_list: List[Dict], req_rate: float, width: int, height: int) -> None:
-    pass
+
+    request_generator = RequestGenerator(req_rate, width, height)
+    new_requests = request_generator.maybe_generate(start_t)
+
+    for request in new_requests:
+        request_dict = {
+            "id": request.id,
+            "pickup": {"x": request.pickup.x, "y": request.pickup.y},
+            "dropoff": {"x": request.dropoff.x, "y": request.dropoff.y},
+            "creation_time": request.creation_time,
+            "status": request.status
+        }
+        out_list.append(request_dict)
 
 def init_state(drivers: List[Driver], requests: List[Request], timeout: int, req_rate: float, width: int, height: int) -> Dict:
     return ADAPTER.init_state(drivers, requests, timeout, req_rate, width, height)
